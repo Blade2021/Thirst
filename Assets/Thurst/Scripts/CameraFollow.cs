453ad91189aa602627372713ecdbf6dcbf7b8076
Sofreshjr <bobjohn820@yahoo.com>
2017-05-30 22:49:27 -0400

    Add files via upload

---
+++ Assets/Thurst/Scripts/CameraFollow.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraFollow : MonoBehaviour {
+	
+	public float slowTime = 0.05f;
+	private Vector3 speed = Vector3.zero;
+	public Transform Character;
+	private Camera cam;
+
+	void Start () {
+		cam = GetComponent<Camera> ();
+		
+		
+	}
+
+	private void FixedUpdate (){
+		if (Character) {
+
+			Vector3 point = cam.WorldToViewportPoint (Character.position);
+			Vector3 delta = Character.position - cam.ViewportToWorldPoint (new Vector3 (0.5f, 0.5f, point.z));
+			Vector3 destination = transform.position + delta;
+			transform.position = Vector3.SmoothDamp (transform.position, destination, ref speed, slowTime);
+		}
+	}
+	
+	void Update () {
+
+
+
+	}
+}
---
+++ Assets/Thurst/Scripts/Walk.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+public class Walk : MonoBehaviour {
+
+	private Animator anim;
+	public float speed;
+	public bool isMoving;
+	private Vector2 wasThere;
+	//public Rigidbody2D rigid;
+
+	void Start () {
+
+		anim = GetComponent<Animator> ();
+		
+	}
+	
+	void Update () {
+
+		isMoving = false;
+
+		if (Input.GetAxisRaw ("Horizontal") > 0.5f || Input.GetAxisRaw ("Horizontal") < -0.5f) {
+			transform.Translate (new Vector3(Input.GetAxisRaw ("Horizontal") * speed * Time.deltaTime, 0f, 0f));
+			//rigid.velocity = sideways * speed;
+			wasThere = new Vector2 (Input.GetAxisRaw ("Horizontal"), 0f);
+			isMoving = true;
+		}
+
+		if (Input.GetAxisRaw ("Vertical") > 0.5f || Input.GetAxisRaw ("Vertical") < -0.5f) {
+			transform.Translate (new Vector3(0f,Input.GetAxisRaw ("Vertical") * speed * Time.deltaTime, 0f));
+			//rigid.velocity = longways * speed;
+			wasThere = new Vector2 (0f, Input.GetAxisRaw ("Vertical"));
+			isMoving = true;
+		}
+
+
+		anim.SetBool ("isMoving", isMoving);
+		anim.SetFloat ("xInput", Input.GetAxisRaw ("Horizontal"));
+		anim.SetFloat ("yInput", Input.GetAxisRaw ("Vertical"));
+		anim.SetFloat ("wasTherey", wasThere.y);
+		anim.SetFloat ("wasTherex", wasThere.x);
+	}
+		
+}
