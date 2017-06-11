9351cc230f5a4fc4b121449ae5e2f41d332e75de
Sofreshjr <bobjohn820@yahoo.com>
2017-06-11 18:28:22 -0400

    Add files via upload
    updated animations and camera follow

--- Assets/Thurst/Scripts/CameraFollow.cs
+++ Assets/Thurst/Scripts/CameraFollow.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
 	
-	public float slowTime = 0.05f;
+	public float slowTime = 0.15f;
 	private Vector3 speed = Vector3.zero;
 	public Transform Character;
 	private Camera cam;
@@ -17,6 +17,16 @@ public class CameraFollow : MonoBehaviour {
 	}
 
 	private void FixedUpdate (){
+		//if (Character) {
+
+		//	Vector3 point = cam.WorldToViewportPoint (Character.position);
+		//	Vector3 delta = Character.position - cam.ViewportToWorldPoint (new Vector3 (0.5f, 0.5f, point.z));
+		//	Vector3 destination = transform.position + delta;
+		//	transform.position = Vector3.SmoothDamp (transform.position, destination, ref speed, slowTime);
+		//}
+	}
+	
+	void Update () {
 		if (Character) {
 
 			Vector3 point = cam.WorldToViewportPoint (Character.position);
@@ -24,10 +34,6 @@ public class CameraFollow : MonoBehaviour {
 			Vector3 destination = transform.position + delta;
 			transform.position = Vector3.SmoothDamp (transform.position, destination, ref speed, slowTime);
 		}
-	}
-	
-	void Update () {
-
 
 
 	}
